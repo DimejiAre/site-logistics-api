5e3657e0b5c209a0c484cc7f516fac917c29eb38
DimejiAre <olardi2@gmail.com>
2024-10-02 18:45:05 -0400

    refactor: refactor import-data.ts to reuse logic

--- src/scripts/import-data.ts
+++ src/scripts/import-data.ts
@@ -9,6 +9,47 @@ import { pipeline } from 'stream/promises';
 import { Sequelize } from 'sequelize-typescript';
 import { Logger } from '@nestjs/common';
 
+async function processData(
+  model: any,
+  filePath: string,
+  batchSize: number,
+  sequelize: Sequelize,
+  logger: Logger,
+  dataLabel: string,
+) {
+  const batch: any[] = [];
+  const transaction = await sequelize.transaction();
+
+  try {
+    await pipeline(
+      fs.createReadStream(filePath),
+      JSONStream.parse('*'),
+      async (stream) => {
+        for await (const item of stream) {
+          batch.push(item);
+          if (batch.length === batchSize) {
+            await model.bulkCreate(batch, { transaction });
+            batch.length = 0;
+          }
+        }
+
+        if (batch.length > 0) {
+          await model.bulkCreate(batch, { transaction });
+        }
+      },
+    );
+
+    await transaction.commit();
+    logger.log(`${dataLabel} inserted successfully.`);
+  } catch (error) {
+    await transaction.rollback();
+    logger.error(
+      `Error inserting ${dataLabel} data, transaction rolled back:`,
+      error,
+    );
+  }
+}
+
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const logger = new Logger('ImportData.ts');
@@ -17,71 +58,26 @@ async function bootstrap() {
   const siteModel = app.get(getModelToken(Site));
   const truckModel = app.get(getModelToken(Truck));
   const batchSize = 1000;
-  const siteBatch: any[] = [];
-  const truckBatch: any[] = [];
 
-  try {
-    const siteTransaction = await sequelize.transaction();
+  await processData(
+    siteModel,
+    'SitesJSONData.json',
+    batchSize,
+    sequelize,
+    logger,
+    'Sites',
+  );
 
-    await pipeline(
-      fs.createReadStream('SitesJSONData.json'),
-      JSONStream.parse('*'),
-      async (siteStream) => {
-        for await (const site of siteStream) {
-          siteBatch.push(site);
-          if (siteBatch.length === batchSize) {
-            await siteModel.bulkCreate(siteBatch, {
-              transaction: siteTransaction,
-            });
-            siteBatch.length = 0;
-          }
-        }
-        if (siteBatch.length > 0)
-          await siteModel.bulkCreate(siteBatch, {
-            transaction: siteTransaction,
-          });
-      },
-    );
+  await processData(
+    truckModel,
+    'TrucksJSONData.json',
+    batchSize,
+    sequelize,
+    logger,
+    'Trucks',
+  );
 
-    await siteTransaction.commit();
-    logger.log('Sites inserted successfully.');
-  } catch (error) {
-    logger.error('Error inserting Sites data, transaction rolled back:', error);
-  }
-
-  try {
-    const truckTransaction = await sequelize.transaction();
-
-    await pipeline(
-      fs.createReadStream('TrucksJSONData.json'),
-      JSONStream.parse('*'),
-      async (truckStream) => {
-        for await (const truck of truckStream) {
-          truckBatch.push(truck);
-          if (truckBatch.length === batchSize) {
-            await truckModel.bulkCreate(truckBatch, {
-              transaction: truckTransaction,
-            });
-            truckBatch.length = 0;
-          }
-        }
-        if (truckBatch.length > 0)
-          await truckModel.bulkCreate(truckBatch, {
-            transaction: truckTransaction,
-          });
-      },
-    );
-
-    await truckTransaction.commit();
-    logger.log('Trucks inserted successfully.');
-  } catch (error) {
-    logger.error(
-      'Error inserting Trucks data, transaction rolled back:',
-      error,
-    );
-  } finally {
-    await app.close();
-  }
+  await app.close();
 }
 
 bootstrap();
